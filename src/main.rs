c87c7aadd9a324c75013f1f50a8299e9cf987647
angle-droit <axelmertz90@gmail.com>
2026-01-06 15:47:07 +0100

    fix: added program loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{fs, io};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    let motd = fs::read_to_string("./src/motd.txt").expect("Something went wrong reading the file");
+    println!("{}", motd);
+
+    while true {
+        println!("user@pentagon~# ");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Something went wrong reading the input");
+
+        match input.as_str() {
+            "test" => println!("{}", motd.trim()),
+            _ => println!("Uknown input"),
+        }
+
+    }
 }
