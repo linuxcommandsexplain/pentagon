3bd52e147877e8148c06f289228016f3ba1e0e9c
angle-droit <axelmertz90@gmail.com>
2026-01-12 23:56:03 +0100

    feat: integrate ratatui for terminal UI and add help command functionality

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,6 +1,22 @@
+use ratatui::layout::Alignment;
+use std::slice::Chunks;
+use ratatui::{Frame, Terminal};
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::widgets::{Block, Borders, Paragraph};
+
 pub fn show_help() {
-    println!("Pentagon help command");
-    println!("  ");
-    println!("<test> for testing command");
-    println!("  ");
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(|mut f| {
+            let pragraph = Paragraph::new("Pentagon help")
+                .alignment(Alignment::Center);
+
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Length(3), Constraint::Min(0)])
+                .split(f.area());
+
+            f.render_widget(pragraph, chunks[0]);
+        }).unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,74 @@
 mod commands;
 
-use std::{fs, io};
-use crate::commands::help::show_help;
-use crate::commands::port_scanner::launch_port_scanner;
+use color_eyre::owo_colors::OwoColorize;
+use color_eyre::Result;
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::{
+    prelude::*,
+    widgets::{Block, Borders, Paragraph},
+};
+use ratatui::widgets::{List, ListItem, ListState};
 
-fn main() {
-    let mut input = String::new();
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let mut terminal = ratatui::init();
+    let mut state = ListState::default();
+    state.select(Some(0));
+    let items = vec!["Help", "Ip scanner"];
 
-    let motd = fs::read_to_string("./src/motd.txt").expect("Something went wrong reading the file");
-    println!("{}", motd);
+    loop {
+        terminal.draw(|mut f| {
+            let area = f.size();
+            let text = "Wolcome to pentagon v2.0";
+            let paragraph = Paragraph::new(text).alignment(Alignment::Center);
 
-    while input != "quit".to_string() {
-        println!("user@pentagon~# ");
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Something went wrong reading the input");
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Length(3), Constraint::Min(0)])
+                .split(f.area());
 
-        match input.trim() {
-            "test" => println!("this is test command"),
-            "quit" => break,
-            "help" => show_help(),
-            "ipscanner" => launch_port_scanner(),
-            _ => println!("Uknown input"),
+            let list = List::new(items.iter().map(|s| ListItem::new(*s)).collect::<Vec<ListItem>>())
+                .block(Block::default().borders(Borders::ALL).title(" Tools "))
+                .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+                .highlight_symbol("> ");
+
+            f.render_widget(
+                Block::default().style(Style::default().bg(Color::Black)),
+                f.area()
+            );
+            f.render_widget(paragraph, chunks[0]);
+            f.render_stateful_widget(list, chunks[1], &mut state)
+        })?;
+
+        if let Event::Key(event) = event::read()? {
+            match event.code {
+                KeyCode::Char('q') => break,
+                KeyCode::Down => {
+                    if let Some(i) = state.selected() {
+                        state.select(Some(i + 1).min(Option::from(items.len() - 1)));
+                    }
+                }
+                KeyCode::Up => {
+                    if let Some(i) = state.selected() {
+                        state.select(Some(i.saturating_sub(1)));
+                    }
+                }
+                KeyCode::Enter => {
+                    if let Some(index) = state.selected() {
+                        match items[index] {
+                            "Help" => {
+                                commands::help::show_help();
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            }
         }
     }
+    ratatui::restore();
+
+    Ok(())
+
 }
